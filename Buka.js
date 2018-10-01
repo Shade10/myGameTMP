5104d8f26c336a7c3743f2a3544478698c300d56
Marcin Kass <marcin.kass10@gmail.com>
2018-10-01 16:52:47 +0200

    buka live and eat

--- Buka.js
+++ Buka.js
@@ -5,8 +5,10 @@ class Buka{
 
         var heroBody = document.createElement('div');
         heroBody.classList.add('hero');
+        heroBody.innerHTML = '<div class="heroEyeLine"><div class ="heroEye"></div><div class ="heroEye"></div></div>';
 
         this.heroBody = heroBody;
+        this.heroEyeLine = heroBody.querySelector('.heroEyeLine');
         world.appendChild(heroBody);
     }
 
@@ -15,6 +17,7 @@ class Buka{
     }
 
     updatePosition(){
+       
         if (this.direction === 'LEFT') {
             this.position.x -= 5;
         }
@@ -24,19 +27,42 @@ class Buka{
         if (this.direction === 'DOWN') {
             this.height = 200;
         }
-        if (this.direction === 'LEFT') {
+        if (this.direction === 'UP') {
             this.height = 400;
         }
-    }
+        
+        document.querySelectorAll('.foodItem').forEach(foodItem => {
+            var foodStyle = window.getComputedStyle(foodItem);
 
+            var x = parseFloat(foodStyle.left);
+            var y = parseFloat(foodStyle.bottom);
+
+            if (y < this.height) {
+                if (x > this.position.x && x < this.position.x + this.width) {
+                    foodItem.remove();
+                    this.width += 10;
+                }
+            }
+        });
+        
+    }
     render(){
         var heroBody = this.heroBody;
-
+        var heroEyeLine = this.heroEyeLine;
+        
         if (this.direction === 'LEFT') {
+            heroEyeLine.style.pagginLeft = '0px';
+            heroEyeLine.style.pagginRight = '40px';
             heroBody.style.marginLeft = this.position.x + 'px';
         }
         if (this.direction === 'RIGHT') {
+            heroEyeLine.style.pagginLeft = '40px';
+            heroEyeLine.style.pagginRight = '0px';
             heroBody.style.marginLeft = this.position.x + 'px';
+        }
+        if (this.direction === 'NONE') {
+            heroEyeLine.style.pagginLeft = '20px';
+            heroEyeLine.style.pagginRight = '20px';
         }
 
         heroBody.style.height = this.height + 'px';
