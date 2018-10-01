c63d33e6da0efb416dc0feb84d5d624259598876
Marcin Kass <marcin.kass10@gmail.com>
2018-10-01 18:44:00 +0200

    buka version 1.0

--- Buka.js
+++ Buka.js
@@ -1,36 +1,37 @@
-class Buka{
-    
-    constructor(world, params){
-        Object.assign(this, params);
+class Buka {
+    constructor(world, params) {
+        Object.assign(this, params)
 
         var heroBody = document.createElement('div');
         heroBody.classList.add('hero');
-        heroBody.innerHTML = '<div class="heroEyeLine"><div class ="heroEye"></div><div class ="heroEye"></div></div>';
-
+        heroBody.innerHTML = '<div class="heroEyeLine"> <div class="heroEye"></div> <div class="heroEye"></div></div>';
+        // heroBody.innerHTML = '<div class="hero__eyeline"><div class="hero__eye"></div><div class="hero__eye"></div></div>';
+        
         this.heroBody = heroBody;
-        this.heroEyeLine = heroBody.querySelector('.heroEyeLine');
-        world.appendChild(heroBody);
+        this.heroEyeLine = heroBody.querySelector('.heroEyeLine')
+
+        world.appendChild(this.heroBody);
     }
+
 
     move(direction){
         this.direction = direction;
     }
 
-    updatePosition(){
-       
-        if (this.direction === 'LEFT') {
-            this.position.x -= 5;
+    updatePosition() {
+        if (this.direction === 'left') {
+          this.position.x -= 5;
         }
-        if (this.direction === 'RIGHT') {
-            this.position.x += 5;
+        if (this.direction === 'right') {
+          this.position.x += 5;
         }
-        if (this.direction === 'DOWN') {
-            this.height = 200;
+        if (this.direction === 'down') {
+          this.height = 100;
         }
-        if (this.direction === 'UP') {
-            this.height = 400;
+        if (this.direction === 'up') {
+          this.height = 200;
         }
-        
+
         document.querySelectorAll('.foodItem').forEach(foodItem => {
             var foodStyle = window.getComputedStyle(foodItem);
 
@@ -38,31 +39,32 @@ class Buka{
             var y = parseFloat(foodStyle.bottom);
 
             if (y < this.height) {
-                if (x > this.position.x && x < this.position.x + this.width) {
+                if (x >= this.position.x && x <= this.position.x + this.width) {
                     foodItem.remove();
                     this.width += 10;
+                    this.height +=2;
                 }
             }
-        });
-        
-    }
+        })
+    }    
+
     render(){
         var heroBody = this.heroBody;
         var heroEyeLine = this.heroEyeLine;
-        
-        if (this.direction === 'LEFT') {
-            heroEyeLine.style.pagginLeft = '0px';
-            heroEyeLine.style.pagginRight = '40px';
+
+        if (this.direction === 'left') {
+            heroEyeLine.style.paddingLeft = '0px';
+            heroEyeLine.style.paddingRight = '40px';
             heroBody.style.marginLeft = this.position.x + 'px';
         }
-        if (this.direction === 'RIGHT') {
-            heroEyeLine.style.pagginLeft = '40px';
-            heroEyeLine.style.pagginRight = '0px';
+        if (this.direction === 'right') {
+            heroEyeLine.style.paddingLeft = '40px';
+            heroEyeLine.style.paddingRight = '0px';
             heroBody.style.marginLeft = this.position.x + 'px';
         }
-        if (this.direction === 'NONE') {
-            heroEyeLine.style.pagginLeft = '20px';
-            heroEyeLine.style.pagginRight = '20px';
+        if (this.direction === 'none') {
+            heroEyeLine.style.paddingLeft = '20px';
+            heroEyeLine.style.paddingRight = '20px';
         }
 
         heroBody.style.height = this.height + 'px';
